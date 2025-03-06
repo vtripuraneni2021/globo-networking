397d41dab9c4250361668fb335365fe792bc2d16
Vijay Tripuraneni <v_tripuraneni@hotmail.com>
2025-03-05 19:41:11 -0800

    Add workspace tag and make variables required

--- resources.tf
+++ resources.tf
@@ -18,6 +18,8 @@ data "aws_availability_zones" "available" {}
 locals {
   common_tags = {
     Environment = var.environment
+    BillingCode = var.billing_code
+    Workspace   = terraform.workspace
   }
 }
 
